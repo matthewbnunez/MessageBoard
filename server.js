fc3be7e4a6a97ff6a456f48e294b181838eeebfe
Matthew Nunez <108908481+matthewbnunez@users.noreply.github.com>
2022-09-20 13:56:24 -0700

    *bugfix (#1)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express();
 const port = 8000;
 
+require('dotenv').config();
 require('./server/config/mongoose.config');
 app.use(cors())
 app.use(express.json());
--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/twitterClone_db", {
+mongoose.connect(process.env.MONGO_CONFIG, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
