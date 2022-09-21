033027b8ba35edfdecb7817d7443168024ca1a55
jonathan fong <jonathan1218@gmail.com>
2022-09-21 15:25:26 -0700

    cleaned up routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,8 @@
 const UserController = require('../controllers/user.controller');
 const PostController = require('../controllers/post.controller');
 const {authenticate} = require('../config/jwt.config')
-<<<<<<< HEAD
-const {authenticate} = require('../config/jwt.config')
-=======
 const PostController = require('../controllers/post.controller');
->>>>>>> 9daf97bbd81f33403a502cdd62dee52ca267b7e5
+
 module.exports = function(app){
     app.get('/api', UserController.index);
     app.post('/api/users', UserController.createUser);
