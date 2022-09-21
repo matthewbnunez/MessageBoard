0793aadc41b0974920b7c327807df1c68642e871
jonathan fong <jonathan1218@gmail.com>
2022-09-21 15:12:28 -0700

    initial commit

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,6 @@
 const UserController = require('../controllers/user.controller');
+const {authenticate} = require('../config/jwt.config')
+
 module.exports = function(app){
     app.get('/api', UserController.index);
     app.post('/api/user', UserController.createUser);
