cdbfa117697ed98cc7d932ae17e7967c49e3a2de
Jfong1218 <109251936+Jfong1218@users.noreply.github.com>
2022-09-21 15:23:56 -0700

    initial commit (#3)

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = 8000;
-
 require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
+
+// config mongoose
 require('./server/config/mongoose.config');
-app.use(cors())
+
+//config express
+app.use(cors( {
+    // credentials: true,
+    // origin:'http://localhost:3000'
+}))
+//POST method
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); //this is new, to use with cookie
+
+//routes
 require('./server/routes/user.routes')(app);
+
+//port
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const UserSchema = new mongoose.Schema({
     userName: { 
         type: String,
@@ -15,11 +17,37 @@ const UserSchema = new mongoose.Schema({
         required: [true, "Last Name is required"],
         minlength: [3, "Last Name must be at least 3 characters"]
     },
+    email:{
+        type:String,
+        required: [true, "Email is required"],
+        validate: {
+            validator: val=> /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
+    },
     password: { 
         type: String,
         required: [true, "Password is required"],
         minlength: [8, "Password must be at least 8 characters"]
     }
 }, { timestamps: true });
+
+//validator to check password and confirmpasswword
+UserSchema.pre('validate', function(next){
+    if(this.password !== this.get('confirmPassword')){
+        this.invalidate('confirmPassword', 'Password must match confirm password')
+    }
+    next()
+})
+
+//saving password hash with bcrypt
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+        this.password = hash;
+        next();
+        });
+});
+
 module.exports.User = mongoose.model('User', UserSchema);
 
